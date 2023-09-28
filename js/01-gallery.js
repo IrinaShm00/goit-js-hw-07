479c550f49eb3fd354e5f520c09f46638dfcd2db
IrinaShm00 <irynashmonina@gmail.com>
2023-09-28 18:03:44 +0200

    change2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector('.gallery');
-gallery.addEventListener("click", handleCklick);
+gallery.addEventListener("click", handleClick);
 
 function createMarkup(arr) {
   return arr
@@ -15,9 +15,25 @@ function createMarkup(arr) {
     .join('');
 }
 
-gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 
-function handleCklick(event) {
+function handleClick(event) {
     event.preventDefault();
-    console.log(event.target);
+    if (event.target === event.currentTarget) {
+        return;
+    }
+    const img = event.target.closest(".gallery__image");
+    console.log(img);
+    
+    const instance = basicLightbox.create(`
+    <div class="modal"> 
+    <img 
+    class="gallery_item"
+    src="${img.preview}"
+    data-source="${img.original}"
+    alt="${img.description}"
+    />
+    </div>
+`)
+instance.show()
 }
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
