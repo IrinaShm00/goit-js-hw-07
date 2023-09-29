8c9d23ec2a2688281bfb0601e733a11622f74efd
IrinaShm00 <irynashmonina@gmail.com>
2023-09-29 11:54:51 +0200

    change3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -17,23 +17,38 @@ function createMarkup(arr) {
 
 
 function handleClick(event) {
+    console.log(event)
     event.preventDefault();
-    if (event.target === event.currentTarget) {
-        return;
-    }
-    const img = event.target.closest(".gallery__image");
-    console.log(img);
-    
-    const instance = basicLightbox.create(`
-    <div class="modal"> 
-    <img 
-    class="gallery_item"
-    src="${img.preview}"
-    data-source="${img.original}"
-    alt="${img.description}"
-    />
-    </div>
-`)
+
+if (typeof event.target.dataset.source === "undefined") {
+    alert("Ooops look like data set is missed!");
+    return;
+}
+
+const instance = basicLightbox.create(`<div class="modal">
+<img class="gallery_item" src="${event.target.dataset.source}"/>
+</div>`)
 instance.show()
 }
-gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+// gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+
+//1 спосіб який не спрацював (передивитися)
+
+//     if (event.target === event.currentTarget) {
+//         return;
+//     }
+//     const img = event.target.closest(".gallery__image");
+//     console.log(img);
+
+//     const instance = basicLightbox.create(`
+//     <div class="modal"> 
+//     <img 
+//     class="gallery_item"
+//     src="${img.preview}"
+//     data-source="${img.original}"
+//     alt="${img.description}"
+//     />
+//     </div>
+// `)
+// instance.show()
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener("click", handleClick);
 
-console.log(galleryItems);
+function createMarkup(arr) {
+  return arr
+    .map(({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>
+    `)
+    .join('');
+}
+// console.log(galleryItems);
+
+function handleClick(event) {
+    console.log(event)
+    event.preventDefault();
+
+if (event.target.classList.contains('gallery__image')) {
+    const gallery = new SimpleLightbox('.gallery a', {
+        captions: true, //показує підписи для img 
+        captionDelay: 250, //adds a delay before the caption shows (in ms)
+        captionsData: 'alt',
+    });
+  }
+}
